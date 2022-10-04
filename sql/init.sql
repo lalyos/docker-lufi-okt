67e791199e42a73c5428ce8513adef6bda97cc73
lalyos <lalyos@yahoo.com>
2022-10-04 05:52:30 +0200

    add init.sql

---
+++ sql/init.sql
@@ -0,0 +1,5 @@
+create TABLE VIP (name varchar(20), age int, money int);
+insert into VIP values ('vladimir', 65, 100);
+insert into VIP values ('bill', 67, 105);
+insert into VIP values ('victor', 68, 1);
+insert into VIP values ('lorinc', 65, 88);
